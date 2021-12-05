5e46a24b1277506c91c89499797d1a665f93bb1e
Thomas Gotwig <tgotwig@gmail.com>
2021-12-05 15:13:02 +0100

    🧪 Add hasPassed & hasNotPassed asserts to happyPath test

--- src/test/java/org/camunda/bpm/getstarted/loanapproval/delegates/MainDelegateTest.java
+++ src/test/java/org/camunda/bpm/getstarted/loanapproval/delegates/MainDelegateTest.java
@@ -1,10 +1,21 @@
 package org.camunda.bpm.getstarted.loanapproval.delegates;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
+
+@SpringBootTest
 class MainDelegateTest {
+
+    @Autowired private RuntimeService runtimeService;
 
     @Test
     public void happyPath() {
+        ProcessInstance pi = runtimeService.startProcessInstanceByKey("loanApproval");
+        assertThat(pi).hasPassed("StartEvent_1").hasNotPassed("Task_0dfv74n");
     }
 }
